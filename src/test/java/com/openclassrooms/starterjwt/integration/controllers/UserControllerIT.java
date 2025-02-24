d2c3d962beba712e23e21ede54e63018d0154d21
AllanLNY <allan1411lannoy@gmail.com>
2025-02-24 20:23:42 +0100

    Adds H2 database dependency and test profile
    Incorporates the H2 database dependency for testing purposes. Also includes the 'test' profile in the test controllers to enable specific test configurations.
    
    Fixes #789

--- src/test/java/com/openclassrooms/starterjwt/integration/controllers/UserControllerIT.java
+++ src/test/java/com/openclassrooms/starterjwt/integration/controllers/UserControllerIT.java
@@ -11,6 +11,7 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 
 import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Optional;
@@ -21,6 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class UserControllerIT {
 
     @Autowired
